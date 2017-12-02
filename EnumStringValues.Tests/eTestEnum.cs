509f11ba6590fcdb5df0476b9c7509d6bb982c8d
Christian Munoz <fushinoryuu@gmail.com>
2017-12-01 20:33:50 -0700

    Deleted the old project and copied over the tests to the new project.

---
+++ EnumStringValues.Tests/eTestEnum.cs
@@ -0,0 +1,25 @@
+﻿namespace EnumStringValues.Tests
+{
+    public enum eTestEnum
+    {
+        Unlabelled,
+
+        [StringValue("1")]
+        SingleDefined,
+
+        [StringValue("2", true)]
+        SingleDefinedWithPreferences,
+
+        [StringValue("3"),
+         StringValue("Three")]
+        MultiDefined,
+
+        [StringValue("4", true),
+         StringValue("Four")]
+        MultiDefinedWithPreferences,
+
+        [StringValue("5", true),
+         StringValue("Five", true)]
+        MultiDefinedWithMultiplePreferences
+    }
+}
