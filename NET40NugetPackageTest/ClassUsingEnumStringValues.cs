2fb1a62732e6b3653b49b27b26f00cd9d43b71fb
Brondahl <stixmclean@googlemail.com>
2017-12-09 10:30:15 +0000

    Add Projects to test package on multiple .NET versions

---
+++ NET40NugetPackageTest/ClassUsingEnumStringValues.cs
@@ -0,0 +1,22 @@
+﻿using EnumStringValues;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace NET40NugetPackageTest
+{
+  [TestFixture]
+  public class ClassUsingEnumStringValues
+  {
+    [Test]
+    public void IsFunctional()
+    {
+      Test.Entry.GetStringValue().Should().Be("Value");
+    }
+
+    public enum Test
+    {
+      [StringValue("Value")]
+      Entry
+    }
+  }
+}
